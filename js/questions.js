5fa0cb28bc254d324be7f810b2346fc8c433c97e
Marlou Rentucan <93507865+MarketValidation@users.noreply.github.com>
2022-01-09 19:48:35 +0800

    V1 january

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Does your product/service concept addresses unemployment rate?",
+    detailedquestion: "Does the product/service provides enough employment in any way that could have the potential to alleviate the unemployment rate in your community?",
+    scoringnote: "Scoring note: the higher the employment opportunities that the product/service concept can provide, the higher the score. ",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Does your product/service concept applicable for individuals that have special needs?",
+    detailedquestion: "Example: for persons with disabilities, pregnant women, senior citizens",
+    scoringnote: "Scoring note: The more individuals that can potentially use the product/service, the higher the score",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Does your product/service concept applicable for all ages? ",
+    detailedquestion: "Do you think your product/service can be used or availed by the general population, is there minimum age restrictions?, can the children use it safely?, does adults or teens can avail it?",
+    scoringnote: "Scoring note: The more individuals that can potentially use the product/service, the higher the score",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Does your product/service concept applicable for most gender types? ",
+    detailedquestion: "Does the product/service concept applicable only for 1 gender group or can it also be used towards other groups i.e Male/Female/LGBT's ? etc.",
+    scoringnote: "Scoring note: The more gender diversity that can potentially use the product/service, the higher the score",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Does your product/service concept address environmental issues?",
+    detailedquestion: "Do you think your product/service can help the environment positively in any way as a result of rendering or providing this product/service concept?",
+    scoringnote: "Scoring note: If you think that your product/service promotes greener, helps, or sustains the environment in any way, the higher the score",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
