c0d522a96e4cd6c6162379f1ee9114e04df1fbfb
Marlou Rentucan <93507865+MarketValidation@users.noreply.github.com>
2022-02-12 15:55:08 +0800

    questions update v1

--- js/questionsTech.js
+++ js/questionsTech.js
@@ -3,9 +3,9 @@
 let questions = [
     {
     numb: 1,
-    question: "Does your product/service concept addresses unemployment rate?",
-    detailedquestion: "Does the product/service provides enough employment in any way that could have the potential to alleviate the unemployment rate in your community?",
-    scoringnote: "Scoring note: the higher the employment opportunities that the product/service concept can provide, the higher the score.",
+    question: "Does your product/service address the unemployment rate?",
+    detailedquestion: "Does the product/service provide enough employment in any way that could have the potential to alleviate the unemployment rate in your community?",
+    scoringnote: "Scoring note: the higher the employment opportunities, the higher the score.",
     answer: .6,
     options: [
       "1",
@@ -24,7 +24,7 @@ let questions = [
     numb: 2,
     question: "Does your product/service concept applicable for individuals that have special needs?",
     detailedquestion: "Example: for persons with disabilities, pregnant women, senior citizens",
-    scoringnote: "Scoring note: The more individuals that can potentially use the product/service, the higher the score.",
+    scoringnote: "Scoring note: The more individuals that can use the product/service, the higher the score.",
     answer: .8,
     options: [
       "1",
@@ -41,9 +41,9 @@ let questions = [
   },
     {
     numb: 3,
-    question: "Does your product/service concept applicable for all ages? ",
-    detailedquestion: "Do you think your product/service can be used or availed by the general population, is there minimum age restrictions?, can the children use it safely?, does adults or teens can avail it?",
-    scoringnote: "Scoring note: The more individuals that can potentially use the product/service, the higher the score.",
+    question: "Is the product/service applicable for all ages?",
+    detailedquestion: "Is your product/service designed to be used by people of all ages?  Or, is there a minimum age restriction? Can children use it safely?",
+    scoringnote: "Scoring note: The more individuals that can use the product/service, the higher the score.",
     answer: .8,
     options: [
       "1",
@@ -60,9 +60,9 @@ let questions = [
   },
     {
     numb: 4,
-    question: "Does your product/service concept applicable for most gender types? ",
-    detailedquestion: "Does the product/service concept applicable only for 1 gender group or can it also be used towards other groups i.e Male/Female/LGBT's ? etc.",
-    scoringnote: "Scoring note: The more gender diversity that can potentially use the product/service, the higher the score.",
+    question: "Is the product/service concept applicable for most gender types? ",
+    detailedquestion: "Is the product/service concept designed only for 1 specific gender group or can it also be used by other groups i.e Male/Female/LGBTQ/non-Binary, etc.?",
+    scoringnote: "Scoring note: The more gender types that can use the product/service, the higher the score.",
     answer: .6,
     options: [
       "1",
@@ -80,8 +80,8 @@ let questions = [
     {
     numb: 5,
     question: "Does your product/service concept address environmental issues?",
-    detailedquestion: "Do you think your product/service can help the environment positively in any way as a result of rendering or providing this product/service concept?",
-    scoringnote: "Scoring note: If you think that your product/service promotes greener, helps, or sustains the environment in any way, the higher the score.",
+    detailedquestion: "Is your product/service designed to address climate change, environmental degradation, or other aspects of environmental protection? Does it use an environmentally friendly business model or manufacturing process?",
+    scoringnote: "Scoring note: If you think that your product/service promotes greener business practices, helps or sustains the natural environment, the higher the score.",
     answer: 1,
     options: [
       "1",
